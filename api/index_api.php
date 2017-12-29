6f14651ee72aedaf04c6a474743451957489de56
Andraž Zvonar <andrazvonar@me.com>
2017-12-29 20:53:33 +0100

    Display all store products on homepage

---
+++ api/index_api.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andraz
+ * Date: 28/12/2017
+ * Time: 19:05
+ */
+
+require_once 'dbconn.php';
+
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$query = "SELECT * FROM article";
+$articles = mysqli_query($conn, $query);
+
+
+echo '<div class="row top-buffer">';
+$i = 0;
+while ($row = mysqli_fetch_array($articles))
+{
+    $i++;
+    if ($i % 4 == 0)
+    {
+        echo '</div><div class="row top-buffer">';
+    }
+    echo '<div class="col-md-4">
+            <div class="article">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="images/'. $row["picture"] .'" class="article-img">
+                    </div>
+                <div class="col-md-6">
+                    <h3>'. $row["name"] .'</h3>
+                    <p>'. $row["description"] .'</p>
+                    <span><b>Cena: '. $row["price"] .'€</b></span>
+                    <hr>
+                    <button type="button" class="btn btn-default">Dodaj v košarico</button>
+                </div>
+               </div>
+            </div>
+          </div>';
+}
+echo "</div>";
--- client/index.php
+++ client/index.php
@@ -6,15 +6,13 @@
     include('head.php');
     include_once('../api/logout_api.php');
 ?>
+
 <body>
 
     <?php include('navbar.php'); ?>
 
     <div class="container">
-        <div class="starter-template">
-            <h1>Spletna trgovina</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-        </div>
+        <?php include('../api/index_api.php'); ?>
     </div><!-- /.container -->
 </body>
 </html>
