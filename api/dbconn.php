b29857049985f4e8a081f1b101f6e8088d991827
lukat33 <lukatavcer@yahoo.com>
2017-12-29 19:13:01 +0100

    Error404 page add

--- api/dbconn.php
+++ api/dbconn.php
@@ -15,10 +15,8 @@ $dbName = "ep_db";
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 mysqli_set_charset($conn, 'utf8');
 
-// Check connection
+//// Check connection
 if (mysqli_connect_errno()) {
+    header("Location: ../client/error404.php");
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-else {
-//    echo  mysqli_get_host_info($conn);
 }
