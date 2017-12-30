60b04d6279cede251c12c314075ead67a6537f2b
Andraž Zvonar <andrazvonar@me.com>
2017-12-30 11:22:08 +0100

    Add article view

---
+++ client/article.php
@@ -0,0 +1,24 @@
+<html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andraz
+ * Date: 30/12/2017
+ * Time: 11:14
+ */
+$title = 'Domov';
+$currentPage = 'index';
+$page_type = 'public';
+include('head.php');
+include_once('../api/logout_api.php');
+?>
+
+
+    <body>
+        <?php include('navbar.php'); ?>
+
+        <div class="container">
+           <?php include('../api/article_api.php'); ?>
+        </div>
+    </body>
+</html>
