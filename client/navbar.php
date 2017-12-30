5b7c3252e5693f50d78bf5bc1d916ea704bc7ac6
lukat33 <lukatavcer@yahoo.com>
2017-12-30 12:47:31 +0100

    Navbar active page fix

--- client/navbar.php
+++ client/navbar.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -6,22 +7,21 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item <?php if ($currentPage == "index") echo "active"; else echo ""; ?>">
                 <a class="nav-link" href="index.php">Domov <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Izdelki</a>
             </li>
 
             <?php
                 if (isset($_SESSION['u_id'])) {
+                    if ($currentPage == "myprofile") $active = "active"; else $active = "";
                     echo
-                        '<li class="nav-item">
+                        '<li class="nav-item '.$active.'">
                             <a class="nav-link" href="myprofile.php">Moj profil</a>
                          </li>';
                     if ($_SESSION['u_role'] == "admin") {
+                        if ($currentPage == "adminpanel") $active = "active"; else $active = "";
                         echo
-                        '<li class="nav-item">
+                        '<li class="nav-item '.$active.'">
                             <a class="nav-link" href="adminpanel.php">Admin</a>
                          </li>';
                     }
