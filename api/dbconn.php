6eeaefa20ed1a555921417a42b39f83970882aea
Andraž Zvonar <andrazvonar@me.com>
2017-12-30 11:11:54 +0100

    Disable error pages

--- api/dbconn.php
+++ api/dbconn.php
@@ -17,7 +17,7 @@ mysqli_set_charset($conn, 'utf8');
 
 // Check connection
 if (mysqli_connect_errno()) {
-    header("Location: ../client/error404.php");
-//    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    //header("Location: ../client/error404.php");
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 }
