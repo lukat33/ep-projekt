a484c4a5a2215b1e7619197c235cb1aa57eae6e0
Andraž Zvonar <andrazvonar@me.com>
2017-12-30 12:40:42 +0100

    Create basket template

--- api/basket_api.php
+++ api/basket_api.php
@@ -5,3 +5,25 @@
  * Date: 30/12/2017
  * Time: 12:18
  */
+
+
+echo '<tr>
+        <td data-th="Product">
+            <div class="row">
+                <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                <div class="col-sm-10">
+                    <h4 class="nomargin">Product 1</h4>
+                    <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </td>
+        <td data-th="Price">$1.99</td>
+        <td data-th="Quantity">
+            <input type="number" class="form-control text-center" value="1">
+        </td>
+        <td data-th="Subtotal" class="text-center">1.99</td>
+        <td class="actions" data-th="">
+            <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+            <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+        </td>
+    </tr>';
--- client/basket.php
+++ client/basket.php
@@ -1,17 +1,45 @@
 <html>
 <?php
-$title = 'Domov';
-$currentPage = 'index';
-$page_type = 'public';
+$title = 'Košarica';
+$currentPage = 'basket';
+$page_type = 'private';
 include('head.php');
 include_once('../api/logout_api.php');
 ?>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/basket.css">
+    </head>
 
     <body>
         <?php include('navbar.php'); ?>
 
         <div class="container">
-            <?php include('../api/basket_api.php'); ?>
+            <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+            <div class="container">
+                <table id="cart" class="table table-hover table-condensed">
+                    <thead>
+                    <tr>
+                        <th style="width:100%">Izdelek</th>
+                        <th style="width:10%">Cena</th>
+                        <th style="width:8%">Količina</th>
+                        <th style="width:22%" class="text-center">Skupna cena</th>
+                        <th style="width:10%"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php include('../api/basket_api.php'); ?>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td><a href="index.php" class="btn tfn-warning"><i class="fa fa-angle-left"></i> Nadaljujte z nakupovanjem</a></td>
+                        <td colspan="2" class="hidden-xs"></td>
+                        <td class="hidden-xs text-center"><strong>Skupaj $1.99</strong></td>
+                        <td><a href="#" class="btn btn-success btn-block">Naprej <i class="fa fa-angle-right"></i></a></td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     </body>
 </html>
